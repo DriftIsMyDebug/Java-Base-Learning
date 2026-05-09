a4f2ef04420bd40768066e98e4a21758af36a260
Waldemar <235098976+DriftIsMyDebug@users.noreply.github.com>
2026-05-10 00:11:52 +0300

    feat: add solutions for 14 level

---
+++ src/level14/Solution1314.java
@@ -0,0 +1,17 @@
+package level14;
+
+public class Solution1314 {
+    public static void main(String[] args) {
+        StringLinkedList2 stringLinkedList2 = new StringLinkedList2();
+        stringLinkedList2.add("1");
+        stringLinkedList2.add("2");
+        stringLinkedList2.add("3");
+        stringLinkedList2.add("4");
+        stringLinkedList2.add("5");
+
+        System.out.println(stringLinkedList2.get(0));
+        System.out.println(stringLinkedList2.get(2));
+        System.out.println(stringLinkedList2.get(4));
+        System.out.println(stringLinkedList2.get(5));
+    }
+}
---
+++ src/level14/StringLinkedList2.java
@@ -0,0 +1,43 @@
+package level14;
+
+public class StringLinkedList2 {
+    private Node first = new Node();
+    private Node last = new Node();
+
+    public StringLinkedList2() {
+        first.next = last;
+        last.prev = first;
+    }
+
+    public void add(String value) {
+        Node newNode = new Node();
+        newNode.value = value;
+
+        Node prevLast = last.prev;
+        prevLast.next = newNode;
+        newNode.prev = prevLast;
+        newNode.next = last;
+        last.prev = newNode;
+    }
+
+    public String get(int index) {
+        Node currentElement = first.next;
+        int currentIndex = 0;
+
+        while (currentElement != null && currentElement != last) {
+            if (currentIndex == index) {
+                return currentElement.value;
+            }
+            currentElement = currentElement.next;
+            currentIndex++;
+        }
+        return null;
+    }
+
+    public static class Node {
+        private Node prev;
+        private String value;
+        private Node next;
+    }
+
+}
