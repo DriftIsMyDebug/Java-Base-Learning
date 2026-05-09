e1727aed0e712d129e0b85fe084785ada0fb7e4b
Waldemar <235098976+DriftIsMyDebug@users.noreply.github.com>
2026-05-10 00:16:11 +0300

    docs: add some comments

--- src/level14/StringLinkedList2.java
+++ src/level14/StringLinkedList2.java
@@ -1,8 +1,12 @@
 package level14;
 
+//Решая эту задачу, ты научишься извлекать элемент из двусвязного списка.
+//Мы реализовали метод add, который добавляет элементы в конец списка. Тебе нужно реализовать метод get(int), который вернет строку под индексом, переданным в метод. Если строки с таким индексом нет, нужно вернуть null.
+//Помни, что first и last не имеют значений, а ссылаются только на первый и последний элемент соответственно.
+
 public class StringLinkedList2 {
-    private Node first = new Node();
-    private Node last = new Node();
+    private final Node first = new Node();
+    private final Node last = new Node();
 
     public StringLinkedList2() {
         first.next = last;
